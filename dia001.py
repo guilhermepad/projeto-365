22a2de234f720b4f612e8bb6e5775bb5c4061d7c
Guilherme Pereira <guialmeida040508@gmail.com>
2026-07-06 14:55:38 -0300

    Dia 1: primeiro programa - calculo de idade

---
+++ dia001.py
@@ -0,0 +1,14 @@
+print("=" * 40)
+print("Projeto 365 - Dia 1")
+print("=" * 40)
+
+nome = input("Qual é o seu nome? ")
+
+idade = int(input("Qual a sua idade? "))
+
+anos = 100 - idade
+
+print()
+
+print(f"Olá, {nome}!")
+print(f"Faltam aproximadamente {anos} anos para você completar 100 anos.")
