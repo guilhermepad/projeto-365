1ac118875ffd1a13dcced310313d71f6c6532394
Guilherme Pereira <guialmeida040508@gmail.com>
2026-07-13 15:31:17 -0300

    Dia 2: ajuste da f-string na temperatura

--- dia002.py
+++ dia002.py
@@ -1,4 +1,4 @@
 cel = input("Me dê a temperatura de hoje: " )
 cel = float(cel)
 
-print(f"A temperatura de hoje em Farhenheit é: {(cel * 1.8) + 32}")
+print(f"A temperatura de hoje em Fahrenheit é: {(cel * 1.8) + 32}")
