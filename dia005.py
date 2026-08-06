5dbfb547ad14772b41f1a99bb9d8f2e60f72398f
Guilherme Pereira <guialmeida040508@gmail.com>
2026-08-06 15:41:11 -0300

    Dia 5: Jogo de advinhação com while e break e contagem de tentativas

--- dia005.py
+++ dia005.py
@@ -1,15 +1,14 @@
-numero_secreto = 7
+numero_secreto = 7 # define o número secreto previamente
 
-tentativas = 0
+tentativas = 0 # define um comço para a contagem das tentativas
 
-while True: 
-    numero = int(input("Digite um número aqui: "))
-    tentativas += 1
-    if numero == numero_secreto:
-        print("Acertou! Parabéns chefe!")
-        print(f"Acertou em {tentativas} tentativas!")
-
-        break
-    print("Errou! Tente novamente!")
+while True: # loop até a condição ser verdadeira
+    numero = int(input("Digite um número aqui: ")) # pede um número e guarda ele
+    tentativas += 1 # aumenta o número de tentativas a cada loop 
+    if numero == numero_secreto: # condição para finalizar o loop
+        print("Acertou! Parabéns chefe!") # se a condição for verdadeira essa mensagem irá aparecer
+        print(f"Acertou em {tentativas} tentativas!") # pega o número de tentativas e mostra ele
+    break # termina o loop se as condições anteriores forem verdadeiras
+    print("Errou! Tente novamente!") # se a condição anterior for falsa ele cai direto aqui
     
    
