826a4d7871195025ea8acefa3bc488560d07ec39
Guilherme Pereira <guialmeida040508@gmail.com>
2026-08-13 15:06:50 -0300

    Day 6: listas em Python - criação, indexação, for loops

---
+++ dia006.py
@@ -0,0 +1,32 @@
+#idades = [10, 12, 15, 10, 13, 10, 11, 12]
+
+#soma_total = 0
+
+#for idade in idades:
+#  soma_total += idade
+#   print(soma_total)
+
+#contador = 0
+
+#idades = [10, 12, 15, 10, 13, 10, 11, 12]
+
+#for idade in idades:
+#    if idade == 10:
+#        contador += 1
+#print(f"Temos um total de: {contador} alunos com 10 anos!")
+
+
+idades = [10, 12, 15, 10, 13, 10, 11, 12, 15, 16, 14, 13]
+
+maiores = []
+
+for idade in idades:
+    if idade >= 13:
+        maiores.append(idade)
+
+print(f"Temos um total de: {len(maiores)} Desbravadores com 13 anos ou mais!")
+    
+
+
+
+    
